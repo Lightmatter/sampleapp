87b16d9bd373bb91031d78006fd10ad5a3f7cad8
Ben Beecher <Ben@lightmatter.com>
2022-01-08 16:54:17 -0500

    testing preview envrionments

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ npm run build
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+while ! nc -z $(echo $DJANGO_AWS_S3_ENDPOINT_URL | echo "http://minio-pr-11.onrender.com" | cut -d "/" -f 3 -) 80; do echo 'Wait minio to startup...' && sleep 1; done;
+sleep 5
 poetry run python ./scripts/create_bucket.py
