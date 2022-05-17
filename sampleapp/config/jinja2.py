3a39cca90096aebdfba752309576261919d24461
Aidan Gerber <aidan@lightmatter.com>
2022-05-17 10:53:00 -0700

    random chart

--- sampleapp/config/jinja2.py
+++ sampleapp/config/jinja2.py
@@ -16,6 +16,26 @@ options = {
 }
 """
 import datetime
+import random
+from math import ceil, floor
+
+
+def random_chart(
+    num_times: int = 10, num_vars: int = 2, starting_val: int = 100
+) -> list[list[int]]:
+    results = [[starting_val for i in range(num_vars)]]
+    for _time in range(num_times - 1):
+        previous_mean = sum(results[-1]) / num_vars
+        results.append([])
+        for var in range(num_vars):
+            previous_val = (results[-2][var] + previous_mean) / 2
+            results[-1].append(
+                random.randrange(  # noqa
+                    floor(0.67 * previous_val), ceil(1.5 * previous_val)  # noqa
+                )  # noqa
+            )
+    return results
+
 
 options = {
     "constants": {"csrf_cookie_name": "sampleapp"},
@@ -28,5 +48,6 @@ options = {
         "django_htmx_script": "django_htmx.templatetags.django_htmx.django_htmx_script",
         "now": datetime.datetime.utcnow,
         "template_localtime": "django.utils.timezone.template_localtime",
+        "random_chart": random_chart,
     },
 }
